2af52ac221de679b39c90662df94b42e10742b6f
Akhil Narang <akhilnarang.1999@gmail.com>
2017-05-25 11:36:30 +0200

    Theme-Resources: cleanup repo for RR
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- README.md
+++ README.md
@@ -1,27 +1,3 @@
-# DU Scripts #
+# RR Theme Resources
 
-### Build scripts ###
-
-# To use the Nico-Build-Script
-
-git clone this repo
-https://github.com/DirtyUnicorns/DU-Scripts.git
-
-Open up a terminal in a directory where you want to download it and copy/paste this
-
-```bash
-git clone git@github.com:DirtyUnicorns/DU-Scripts.git
-```
-
-Now with your file browser go into the DU-Scripts folder and copy the build_DU.sh file and paste it in your DU root source.
-
-After doing that, you are ready to use the script simply by this way :
-
-Open up a terminal or if you have the old terminal still open, in your DU root source and type this ```. build_DU.sh```
-
-After, simply respond to what is displayed to compile the build.
-
-
-# To use the nathan-build-script
-
- In the root of your DU source, type `bash nathan-build-script.sh --help` to learn about the script.
+### Thanks to DU for the script
--- update-themes.sh
+++ update-themes.sh
@@ -78,35 +78,23 @@ declare -a common=('common.mk' '.classpath' '.git' '.gitignore' '.idea' '.projec
 declare -a packages=('StatementService' 'BackupRestoreConfirmation' 'CaptivePortalLogin' 'DefaultContainerService' 'ExternalStorageProvider' 'FakeOemFeatures' 'FusedLocation' 'InputDevices' 'Keyguard' 'PrintSpooler' 'SettingsProvider'
                      'SharedStorageBackup' 'Shell' 'VpnDialogs' 'WAPPushManager' 'WAPPushManager' 'WallpaperCropper' 'services' 'WallpaperBackup' 'PrintRecommendationService' 'Osu' 'MtpDocumentsProvider' 'ExtShared' 'ExtServices' 'CtsShim')
 
-declare -a res=('Android.mk' 'AndroidManifest.xml' 'MODULE_LICENSE_APACHE2' 'NOTICE' 'lint.xml' 'assets' 'menu*' 'values-*' 'xml*' 'interpolator' 'raw*' 'anim*' 'animator' 'transition' 'MakeJavaSymbols.sed' 'tests' 'src' 'build.gradle' 'proguard.flags' 'setting' 'gradlew' 'gradlew.bat')
+declare -a res=('Android.mk' 'AndroidManifest.xml' 'MODULE_LICENSE_APACHE2' 'NOTICE' 'lint.xml' 'assets' 'menu*' 'values-*' 'xml*' 'interpolator' 'raw*' 'anim*' 'animator' 'transition' 
+'MakeJavaSymbols.sed' 'tests' 'src' 'build.gradle' 'proguard.flags' 'setting' 'gradlew' 'gradlew.bat')
 
 declare -a values=('all_search_engines.xml' 'appmsg_colors.xml' 'arrays.xml' 'attrs_manifest.xml' 'bookmarks_icons.xml' 'bools.xml' 'config.xml' 'defaults.xml' 'donottranslate.xml' 'donottranslate_config.xml'
                    'aliases.xml' 'crop_colors.xml' 'dslv_attrs.xml' 'animation_constants.xml' 'donottranslate-cldr.xml' 'donottranslate-maps.xml' 'donottranslate-names.xml' 'donottranslate_material.xml'
-                   'donottranslate-search_engines.xml' 'du_arrays.xml' 'du_strings.xml' 'du_symbols.xml' 'fractions.xml' 'ids.xml' 'integers.xml' 'internal.xml' 'keys.xml' 'lland_config.xml' 'lland_strings.xml' 'plurals.xml'
+                   'donottranslate-search_engines.xml' 'rr_arrays.xml' 'rr_strings.xml' 'rr_symbols.xml' 'fractions.xml' 'ids.xml' 'integers.xml' 'internal.xml' 'keys.xml' 'lland_config.xml' 'lland_strings.xml' 'plurals.xml'
                    'public.xml' 'strings.xml' 'symbols.xml' 'vpi_attrs.xml' 'vpi_defaults.xml' 'cm_arrays.xml' 'cm_plurals.xml' 'cm_strings.xml' 'custom_strings.xml' 'custom_arrays.xml' 'vpi__defaults.xml' 'vpi__attrs.xml'
                    'nontranslatables.xml' 'accountprovider.xml' 'constants.xml' 'codeaurora_strings.xml' 'filtershow_ids.xml' 'filtershow_strings.xml' 'filtershow_values.xml' 'filtershow_values_attrs.xml')
 
 declare -a core=('java' 'jni' 'tests' 'src')
 
-declare -a theme_packages=('Calculator' 'Calendar' 'CalendarWidget' 'Camera2' 'Contacts' 'ContactsCommon' 'DUI' 'DU-Tweaks' 'DeskClock' 'Dialer' 'Gallery2'
-                           'Launcher3' 'Messaging' 'MusicFX' 'OmniSwitch' 'PhoneCommon' 'Settings' 'Stk')
+declare -a theme_packages=('Calculator' 'Calendar' 'CalendarWidget' 'Camera2' 'Contacts' 'ContactsCommon' 'DUI' 'DeskClock' 'Dialer' 'Gallery2'
+                           'Launcher3' 'Messaging' 'MusicFX' 'OmniSwitch' 'PhoneCommon' 'Settings' 'Stk' 'ResurrectionOTA' 'ResurrectionStats' )
 
 declare -a extra=('java' '.idea' 'gradle' '.gitignore' 'build.gradle' 'gradlew' 'gradlew.bat' 'local.properties' 'proguard-rules.pro' 'proguard.flags')
 
 cd $WORKING_DIR
-echo "Removing files so we can clean sync"
-delete_useless ${root[@]}
-cd .repo
-rm -rf local_manifests
-cd ..
-echo "Repo Syncing........."
-repo sync --force-sync >> /dev/null
-if [ $? -eq 0 ]; then
-  echo "Repo Sync success"
-else
-  echo "Repo Sync failure"
-  exit 1
-fi
 echo "Removing unneeded files"
 cd frameworks/base
 delete_useless ${frameworks[@]}
@@ -147,21 +135,13 @@ cd values
 delete_useless ${values[@]}
 rm -rf  $WORKING_DIR/vendor/scripts/themes-resources/frameworks
 cd $WORKING_DIR
-cd packages/apps/DU-Updater
-delete_useless ${common[@]}
-cd $WORKING_DIR/packages/apps/DU-Updater/app/src/main
-delete_useless ${extra[@]}
-cd $WORKING_DIR/packages/apps/DU-Updater/app/
-delete_useless ${extra[@]}
-cd $WORKING_DIR
 process_all ${theme_packages[@]}
 echo "Cleaning target folders"
 rm -rf vendor/scripts/themes-resources/packages
 mkdir vendor/scripts/themes-resources/packages
 mkdir vendor/scripts/themes-resources/packages/apps
 mkdir vendor/scripts/themes-resources/frameworks
-echo "Copying all files to $WORKING_DIR/DU-Scripts/themes-resources"
-cp -r $WORKING_DIR/packages/apps/DU-Updater $WORKING_DIR/vendor/scripts/themes-resources/packages/apps/
+echo "Copying all files to $WORKING_DIR/vendor/scripts/themes-resources"
 copy_all ${theme_packages[@]}
 cp -r $WORKING_DIR/frameworks/base/core $WORKING_DIR/frameworks/base/packages $WORKING_DIR/vendor/scripts/themes-resources/frameworks
 cd $WORKING_DIR/vendor/scripts/themes-resources/packages/apps/Dialer/InCallUI
@@ -182,7 +162,12 @@ cd $WORKING_DIR/vendor/scripts/themes-resources
 sleep 2
 rm -rf packages/apps/Camera2/proguard-project.txt
 sleep 2
-rm -rf packages/apps/DU-Updater/app/src/main/Android.mk
-sleep 2
-rm -rf packages/apps/DU-Updater/app/src/main/res/values/strings.xml
-sleep 2
+
+cd vendor/scripts
+git add -A
+echo "Cleaning java, makefiles, etc."
+rm -rfv $(find -name *.java -o -name *.pro -o -name *.gitignore -o -name *.gradle -o -name *.mk)
+echo "ResurrectionRemix Theme Resources Update on $(date)" >> /tmp/thememsg
+git commit -asF /tmp/thememsg
+rm -fv /tmp/thememsg
+git push origin nougat -u
